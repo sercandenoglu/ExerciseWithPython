cfa0ba54c5935dced395930248d919d0992941ad
Sercan <63645223+sercandenoglu@users.noreply.github.com>
2021-03-21 06:06:54 +0300

    Update Affine_Chipher.py

--- Affine_Chipher.py
+++ Affine_Chipher.py
@@ -13,7 +13,7 @@ def GetTextandKey():
 camelAlphabet = "ABCÇDEFGĞHİIJKLMNOÖPQRSŞTUÜVWXYZ"#also you can use this code string.ascii_lowercase for english camelAlphabet but first you have to import the string module
 lowerAlphabet = "abcçdefgğhiıjklmnoöpqrsştuüvwxyz"#also you can use camelAlphabet.lower(), I didn't use because sometimes that give error for turkish alphabet
 alphabets = camelAlphabet + lowerAlphabet
-plainText, a, b = "lorem ipsum LOREm-İpğsum", 2, 3#GetTextandKey()
+plainText, a, b = GetTextandKey()
 
 encryption = lambda letter: camelAlphabet[(a*camelAlphabet.index(letter)+b)%len(camelAlphabet)] if letter in camelAlphabet else lowerAlphabet[(a*lowerAlphabet.index(letter)+b)%len(lowerAlphabet)]#Formula is y=a*x+b -> y=new letter, x=old letter
 print("".join((map(lambda letter: encryption(letter) if letter in alphabets else letter, plainText))))

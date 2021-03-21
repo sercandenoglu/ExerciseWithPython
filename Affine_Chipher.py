873dd6f4110f93eff7d14fa1d941d98323a6b02d
Sercan <63645223+sercandenoglu@users.noreply.github.com>
2021-03-21 06:06:24 +0300

    Add files via upload

---
+++ Affine_Chipher.py
@@ -0,0 +1,21 @@
+def GetTextandKey():
+    print("Formula: y=ax+b, x our latter")
+    while True:
+        try:
+            a = int(input("Enter a value:"))
+            b = int(input("Enter b value:"))
+            plainText = input("Enter the text: ")
+            return plainText, a, b
+        except ValueError:
+            print("You can only enter integers for a and b values")
+
+
+camelAlphabet = "ABCÇDEFGĞHİIJKLMNOÖPQRSŞTUÜVWXYZ"#also you can use this code string.ascii_lowercase for english camelAlphabet but first you have to import the string module
+lowerAlphabet = "abcçdefgğhiıjklmnoöpqrsştuüvwxyz"#also you can use camelAlphabet.lower(), I didn't use because sometimes that give error for turkish alphabet
+alphabets = camelAlphabet + lowerAlphabet
+plainText, a, b = "lorem ipsum LOREm-İpğsum", 2, 3#GetTextandKey()
+
+encryption = lambda letter: camelAlphabet[(a*camelAlphabet.index(letter)+b)%len(camelAlphabet)] if letter in camelAlphabet else lowerAlphabet[(a*lowerAlphabet.index(letter)+b)%len(lowerAlphabet)]#Formula is y=a*x+b -> y=new letter, x=old letter
+print("".join((map(lambda letter: encryption(letter) if letter in alphabets else letter, plainText))))
+
+
